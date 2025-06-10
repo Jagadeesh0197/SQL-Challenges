545ce1967941746e1ee904beb6162115119d8cda
Jagadeesh0197 <111108127+Jagadeesh0197@users.noreply.github.com>
2025-06-11 00:10:20 +0530

    Update Solutions.sql

--- SQLZOO-Solutions/Solutions.sql
+++ SQLZOO-Solutions/Solutions.sql
@@ -301,3 +301,15 @@ SELECT matchid, mdate, COUNT(teamid)
 FROM goal JOIN game ON (matchid=id)
 WHERE teamid='GER'
 GROUP BY matchid, mdate
+
+--13. List every match with the goals scored by each team as shown. This will use "CASE WHEN" 
+-- which has not been explained in any previous exercises. Notice in the query given every goal is listed. 
+-- If it was a team1 goal then a 1 appears in score1, otherwise there is a 0. You could SUM this column 
+-- to get a count of the goals scored by team1. Sort your result by mdate, matchid, team1 and team2.
+SELECT mdate, team1,
+SUM(CASE WHEN teamid=team1 THEN 1 ELSE 0 END) score1,
+team2,
+SUM(CASE WHEN teamid=team2 THEN 1 ELSE 0 END) score2
+	FROM game LEFT OUTER JOIN goal ON id = matchid
+	GROUP BY id
+	ORDER BY mdate, matchid, team1, team2;
