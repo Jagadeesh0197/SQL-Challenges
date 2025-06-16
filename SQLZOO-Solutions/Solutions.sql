33833f2b2a1e64fffce2064667e89db4df7854da
Jagadeesh0197 <111108127+Jagadeesh0197@users.noreply.github.com>
2025-06-16 20:06:34 +0530

    Update Solutions.sql

--- SQLZOO-Solutions/Solutions.sql
+++ SQLZOO-Solutions/Solutions.sql
@@ -313,3 +313,23 @@ SUM(CASE WHEN teamid=team2 THEN 1 ELSE 0 END) score2
 	FROM game LEFT OUTER JOIN goal ON id = matchid
 	GROUP BY id
 	ORDER BY mdate, matchid, team1, team2;
+
+
+-----------------------------------------------7. More JOIN operations--------------------------------------------
+--1. List the films where the yr is 1962 [Show id, title]
+SELECT id, title
+ FROM movie
+ WHERE yr=1962
+
+--2. Give year of 'Citizen Kane'.
+SELECT yr FROM movie
+WHERE title = 'Citizen Kane'
+
+--3. List all of the Star Trek movies, include the id, title and yr (all of these movies include
+--the words Star Trek in the title). Order results by year.
+SELECT id, title, yr
+FROM movie
+WHERE title LIKE '%Star Trek%'
+ORDER BY yr
+
+--4. 
